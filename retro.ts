d60be27ddb0f48a72e8a6912ac89cf19fcdd42e9
ChaseMor <C_Mortensen@live.cm>
2018-10-13 18:11:07 -0700

    Added Retro section to toolbox

---
+++ retro.ts
@@ -0,0 +1,12 @@
+//% color=#9BED0D
+//% weight=50 
+//% icon="\uf11b"
+//% block="Retro"
+namespace retro {
+
+    let retroEnabled: boolean;
+    //% block
+    export function enableRetroArcadeMode() {
+        retroEnabled = true;
+    }
+}
